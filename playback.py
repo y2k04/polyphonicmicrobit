ad503b5484bb982a2a15537cd2e0cc56276b6da8
Y2K4 <29883287+y2k04@users.noreply.github.com>
2026-04-18 16:36:18 +1000

    Small EQ and volume change

--- playback.py
+++ playback.py
@@ -9,10 +9,10 @@ import numpy as np
 import sounddevice as sd
 
 # Audio Settings
-EQ_LOW = 1.4
+EQ_LOW = 1.3
 EQ_MID = 1.5
 EQ_HIGH = 0.05
-VOLUME = 1.0
+VOLUME = 0.6
 
 SAMPLE_RATE = 44100
 CHANNELS = 2
